194e34e203e84ee6028c2ce311377d6b7e574822
DJin24 <davidchristopherjin@gmail.com>
2021-05-27 21:19:34 -0500

    All files

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+mod rust_ge;
+
+use crate::rust_ge::rust_ge_base::Abstract_game;
+
+struct TestGame {}
+
+impl Abstract_game for TestGame {
+
+}
+
+pub fn main() {
+    let testGame = TestGame{};
+    testGame.run();
+}
